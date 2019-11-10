418f2330e11777611b387dfb43add7d7765e8f43
John Isom <John.Isom2001@gmail.com>
2019-11-09 20:59:48 -0700

    Put stop command in own file

---
+++ stop.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from helpers import readlines, sanitize, last_stop
+from constants import FILE_DEST, TIME_FORMAT_PATTERN, MESSAGE_DELIM, EOL
+
+
+def stop(message: str) -> None:
+    '''Add stop time'''
+    if last_stop(readlines()[-1]):
+        print("Cannot 'stop' twice in a row!")
+        return
+    message = sanitize(message)
+    with open(FILE_DEST, 'a') as f:
+        f.write(f'{datetime.now().strftime(TIME_FORMAT_PATTERN)}'
+                f'{MESSAGE_DELIM}{message}{EOL}')
