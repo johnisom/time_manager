71abc56deb159d617dedadabbf47689e42e50bb1
John Isom <John.Isom2001@gmail.com>
2019-11-12 15:56:24 -0700

    Redirect stdout depending on length of output
    If the length of the output in lines is greater than the size of the terminal,
    then all of the standard output is redirected (piped) to the shell 'less'
    command. If not, then it is printed to stdout.

--- time_manager.py
+++ time_manager.py
@@ -5,12 +5,13 @@
 # to a version of python 3.6 or greater.
 
 import sys
+import subprocess
 import os
 
 from lib.run import run
 from lib.display import display_help
 from lib.command_validation import is_valid, is_help
-from constants import PATH_TO_USER
+from lib.constants import PATH_TO_USERS
 
 if __name__ == "__main__":
     # gets the arguments passed in when run as a script/command
@@ -23,4 +24,19 @@ if __name__ == "__main__":
     if not is_valid(args) or is_help(args[0]):
         display_help()
     else:
+        tmp_path = os.environ['HOME'] + '/time_manager/tmp'
+        if not os.path.isdir(tmp_path):
+            os.mkdir(tmp_path)
+        sys.stdout = open(tmp_path + '/stdout.txt', 'w')
         run(*args)
+        sys.stdout.close()
+        sys.stdout = sys.__stdout__
+        with open(tmp_path + '/stdout.txt') as stdout:
+            lines = stdout.readlines()
+            if len(lines) <= os.get_terminal_size().lines:
+                for line in lines:
+                    print(line, end='')
+            else:
+                subprocess.run(['less'], input=''.join(lines).encode('ascii'))
+        os.remove(tmp_path + '/stdout.txt')
+        os.rmdir(tmp_path)
