643272a6df92bf1469d7ea1ebada701b9c014fc2
John Isom <John.Isom2001@gmail.com>
2019-11-19 09:46:09 -0700

    Add colors class/constants to constants.py

--- lib/constants.py
+++ lib/constants.py
@@ -20,3 +20,28 @@ PATH_TO_HELP = os.environ['HOME'] + '/time_manager/help.txt'
 PATH_TO_STDOUT = os.environ['HOME'] + '/time_manager/tmp/stdout.txt'
 PATH_TO_TMP = os.environ['HOME'] + '/time_manager/tmp'
 PATH_TO_USERS = os.environ['HOME'] + '/time_manager/users'
+
+
+class colors:
+
+    RESET = '\u001b[0m'
+
+    class FG:
+        BLK = '\u001b[30m'
+        RED = '\u001b[31m'
+        GRN = '\u001b[32m'
+        YEL = '\u001b[33m'
+        BLU = '\u001b[34m'
+        MAG = '\u001b[35m'
+        CYA = '\u001b[36m'
+        WHI = '\u001b[37m'
+
+    class BG:
+        BLK = '\u001b[40m'
+        RED = '\u001b[41m'
+        GRN = '\u001b[42m'
+        YEL = '\u001b[43m'
+        BLU = '\u001b[44m'
+        MAG = '\u001b[45m'
+        CYA = '\u001b[46m'
+        WHI = '\u001b[47m'
